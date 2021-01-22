58cf68e629c11c79905ac79c9bb2b18f9d6cd25b
Endry <andrijsisae@gmail.com>
2021-01-22 18:06:32 +0200

    db disconnected

--- src/main/java/org/task/TestTaskApplication.java
+++ src/main/java/org/task/TestTaskApplication.java
@@ -2,12 +2,12 @@ package org.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TestTaskApplication {
     public static void main(String args[]){SpringApplication.run(TestTaskApplication.class, args); }
 }
