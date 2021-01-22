bc0d87947a23f7230b0e608f89ff0e49a455543e
Endry <andrijsisae@gmail.com>
2021-01-22 17:24:46 +0200

    all config added

---
+++ src/main/java/org/task/HelloController.java
@@ -0,0 +1,11 @@
+package org.task;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    String hello(){return "Hello World!";}
+}
