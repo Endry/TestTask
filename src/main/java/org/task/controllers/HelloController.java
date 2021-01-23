1e6be39930dfd024539b0520c587c16fda3af7a7
Andrii Sukhetskiy <andrnkep@gmail.com>
2021-01-24 01:54:22 +0200

    The client part is ready, the initial server settings are configured.

--- src/main/java/org/task/controllers/HelloController.java
+++ src/main/java/org/task/controllers/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
     @GetMapping("/index")
-    String hello(){return "index";}
+    public String hello(){return "index";}
 }
