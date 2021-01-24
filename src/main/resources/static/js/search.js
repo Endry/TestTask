c3d9fd2f6d6f9bd9fe3bbf26502ce0c2ed67b8b5
Andrii Sukhetskiy <andrnkep@gmail.com>
2021-01-25 00:19:21 +0200

    Added logging, configured js files(add to html).

---
+++ src/main/resources/static/js/search.js
@@ -0,0 +1,8 @@
+$('.button_st').on('click', function() {
+  $('#bloc_m').animate({opacity:.5});
+  //  $.getJSON("demo_ajax_json.js", function(result){
+    //  $.each(result, function(i, field){
+    //    $("div").append(field + " ");
+    //  });
+  //  });
+});
---
+++ target/classes/static/js/search.js
@@ -0,0 +1,10 @@
+//$(document).ready(function(){
+  $('.button_st').on('click', function() {
+  $('#bloc_m').animate({opacity:.5});
+  //  $.getJSON("demo_ajax_json.js", function(result){
+    //  $.each(result, function(i, field){
+    //    $("div").append(field + " ");
+    //  });
+  //  });
+  });
+//});
