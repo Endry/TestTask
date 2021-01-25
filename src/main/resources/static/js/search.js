6cd49fab7715f1d7e7882539f10e3917534c05ae
Andrii Sukhetskiy <andrnkep@gmail.com>
2021-01-25 22:35:23 +0200

    Create connection server-client, RequestMapping

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,7 +1,7 @@
 function getSearch() {
   $('#bloc_m').animate({opacity:.5});
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'phones.json', false);
+  xhr.open('GET', 'search.json', false);
   xhr.send();
   if (xhr.status != 200) { // обработать ошибку
     alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
